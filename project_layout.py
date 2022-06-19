95e4703b49fb81576f1bbf6de88debb3b383fd18
linth <zhua0404@gmail.com>
2022-06-20 01:54:04 +0800

    add Project Layout example.

---
+++ project_layout.py
@@ -0,0 +1,54 @@
+'''
+Project Layout
+
+/home/user/Projects/flask-tutorial
+├── flaskr/
+│   ├── __init__.py
+│   ├── db.py
+│   ├── schema.sql
+│   ├── auth.py
+│   ├── blog.py
+│   ├── templates/
+│   │   ├── base.html
+│   │   ├── auth/
+│   │   │   ├── login.html
+│   │   │   └── register.html
+│   │   └── blog/
+│   │       ├── create.html
+│   │       ├── index.html
+│   │       └── update.html
+│   └── static/
+│       └── style.css
+├── tests/
+│   ├── conftest.py
+│   ├── data.sql
+│   ├── test_factory.py
+│   ├── test_db.py
+│   ├── test_auth.py
+│   └── test_blog.py
+├── venv/
+├── setup.py
+└── MANIFEST.in
+
+
+.gitignore file:
+```
+venv/
+
+*.pyc
+__pycache__/
+
+instance/
+
+.pytest_cache/
+.coverage
+htmlcov/
+
+dist/
+build/
+*.egg-info/
+```
+
+Reference:
+    - https://flask.palletsprojects.com/en/2.1.x/tutorial/layout/
+'''
